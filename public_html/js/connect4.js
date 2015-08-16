1d431c6570554108a6385104bdd1a5e06a77b540
TankTan38 <iamtanktan@gmail.com>
2015-08-16 18:52:16 -0500

    Restructured the main code, esp. the events

--- public_html/js/connect4.js
+++ public_html/js/connect4.js
@@ -46,71 +46,26 @@ XXX.src = "img/X.png";
  * Main Code
  */
 
+//Prepare the array for the glorious connect 4
 fillArray();
 
 //Draw the board upon load
-$(window).load(function () {
-    ctx2.drawImage(board, 0, 0, bw, bh + (bh / 6));
-});
+$(window).load(drawBoard);
 
 //Ran when the site is fully loaded
 $(document).ready(function () {
+    
     //Ran when user clicks on the canvas
-    $('canvas').click(function (e) {
-        //drop the chip where the user clicked
-        var offset = $(this).offset();
-        var xPos = (e.pageX - offset.left);
-        for (var i = 1; i < 8; i++) {
-            if (((i - 1) * (bw / 7)) < xPos && xPos < ((i) * (bw / 7))) {
-                dropChip(i);
-            }
-        }
-    });
-});
-
-//Also ran when the site is fully loaded
-$(document).ready(function (e) {
+    $('canvas').click(click);
+    
     //called when the mouse moves across the canvas
-    //hovers the cip above the board before being dropped
-    $('canvas').mousemove(function (e) {
-        var offset = $(this).offset();
-        var xPos = (e.pageX - offset.left);
-        var image = new Image();
-        
-        //checks which color's turn it is
-        if (moves % 2 === 0) {
-            image.src = "img/bestchipred.png";
-        }
-        else {
-            image.src = "img/bestchipblue.png";
-        }
-        
-        //draw the image of the chip to be dropped
-        for (var i = 1; i < 8; i++) {
-            if (xPos > ((i - 1) * (bw / 7)) && xPos < (i * (bw / 7)) && winner === "False") {
-                ctx.clearRect(0, -(bh / 6), bw, (bh / 6));
-                ctx.drawImage(image, ((i - 1) * (bw / 7)), -(bh / 6), (bw / 7), (bh / 6));
-            }
-        }
-        
-        //clear all draws of hover chips
-        if (winner === "True" && once === "False") {
-            ctx.clearRect(0, -(bh / 6), bw, (bh / 6));
-            once = "True";
-        }
-    });
+    $('canvas').mousemove(hoverChip);
 });
 
 
 /*
  * Functions
  */
-window.requestAnimFrame = (function (callback) {
-    return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
-            function (callback) {
-                window.setTimeout(callback, 1000 / 60);
-            };
-})();
 
 function drawChip(x, y) {
     var chipColor = redchip;
@@ -124,24 +79,25 @@ function drawChip(x, y) {
     pos_array[x][y] = color;
     x = (bw / 7) * (x - 1);
     y = (bh / 6) * (y - 1);
-
     var chip = {
         x: x,
         y: 0,
         width: (bw / 7),
         height: (bh / 6)
     };
-
     var startTime = (new Date()).getTime();
-
+    window.requestAnimFrame = (function (callback) {
+        return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
+                function (callback) {
+                    window.setTimeout(callback, 1000 / 60);
+                };
+    })();
     function animate(chip, canvas, ctx, startTime) {
         // update
         var time = (new Date()).getTime() - startTime;
-
         var a = 1300;
         // pixels / second
         var newY = (a * Math.pow(time / 1000, 2) - (bh / 6));
-
         if (newY < y) {
             chip.y = newY;
             // request new frame
@@ -155,17 +111,15 @@ function drawChip(x, y) {
 
         // clear
         ctx.clearRect(x, (chip.y - 110), (bw / 7), ((bh / 6) + 30));
-
         ctx.drawImage(chipColor, chip.x, chip.y, chip.width, chip.height);
-
     }
 
-    //initial call, and recurs until the chip drops all the way
+//initial call, and recurs until the chip drops all the way
     animate(chip, canvas, ctx, startTime);
 }
 
 function dropChip(x) {
-    //for loop that checks array starting at bottom of board which is at 6 going up to 1
+//for loop that checks array starting at bottom of board which is at 6 going up to 1
     for (var j = 6; j > 0; j--) {
         if (pos_array[x][j] === undefined && winner === "False") {
             drawChip(x, j);
@@ -194,7 +148,7 @@ function fillArray() {
 
 //[columns][rows]
 function winCondition() {
-    //horizontal
+//horizontal
     for (var i = 1; i < 5; i++) {
         for (var j = 1; j < 7; j++) {
             if (pos_array[i][j] !== undefined && pos_array[i][j] === pos_array[i + 1][j] && pos_array[i][j] === pos_array[i + 2][j] && pos_array[i][j] === pos_array[i + 3][j]) {
@@ -203,7 +157,7 @@ function winCondition() {
         }
     }
 
-    //vertical
+//vertical
     for (var i = 1; i < 8; i++) {
         for (var j = 1; j < 4; j++) {
             if (pos_array[i][j] !== undefined && pos_array[i][j] === pos_array[i][j + 1] && pos_array[i][j] === pos_array[i][j + 2] && pos_array[i][j] === pos_array[i][j + 3]) {
@@ -211,7 +165,7 @@ function winCondition() {
             }
         }
     }
-    // /diagonals
+// /diagonals
     for (var i = 1; i < 5; i++) {
         for (var j = 4; j < 7; j++) {
             if (pos_array[i][j] !== undefined && pos_array[i][j] === pos_array[i + 1][j - 1] && pos_array[i][j] === pos_array[i + 2][j - 2] && pos_array[i][j] === pos_array[i + 3][j - 3]) {
@@ -219,7 +173,7 @@ function winCondition() {
             }
         }
     }
-    // \diagonals
+// \diagonals
     for (var i = 1; i < 5; i++) {
         for (var j = 1; j < 4; j++) {
             if (pos_array[i][j] !== undefined && pos_array[i][j] === pos_array[i + 1][j + 1] && pos_array[i][j] === pos_array[i + 2][j + 2] && pos_array[i][j] === pos_array[i + 3][j + 3]) {
@@ -227,9 +181,9 @@ function winCondition() {
             }
         }
     }
-    // tie
+// tie
     if (moves === 42 && winner === "False") {
-        //manual win event instead of using win function
+//manual win event instead of using win function
         winner = "True";
         setTimeout(function () {
             ctx.drawImage(draw, (3 * bw / 10), -(bh / 6), (bw / 2.5), (bh / 6));
@@ -240,17 +194,15 @@ function winCondition() {
 //i and j are the coord of the first chip in the winning four
 function win(i, j, direction) {
     winner = "True";
-
     //Draw the win pic based on the color of the chip that won after a delay
     setTimeout(drawWinBanner, 500, pos_array[i][j]);
-
     //delay
     setTimeout(drawWinXs, 1000, i, j, direction);
 }
 
 function drawWinBanner(color) {
 
-    //choose the correct picture for either red or blue
+//choose the correct picture for either red or blue
     var temp;
     switch (color) {
         case "red":
@@ -261,14 +213,14 @@ function drawWinBanner(color) {
             break;
     }
 
-    //draw that sucker
+//draw that sucker
     ctx.drawImage(temp, (bw / 6), -(bh / 6), (bw / 1.5), (bh / 6));
 }
 
 function drawWinXs(i, j, direction) {
-    //repeat four times because it's connect FOUR
+//repeat four times because it's connect FOUR
     for (var n = 1; n < 5; n++) {
-        //draw the X
+//draw the X
         ctx.drawImage(XXX, (bw / 7) * (i - 1), (bh / 6) * (j - 1), (bw / 7), (bh / 6));
         //change the coordinate position based on which direction the win was
         switch (direction) {
@@ -287,5 +239,47 @@ function drawWinXs(i, j, direction) {
                 i++;
                 break;
         }
+    }
+}
+
+function drawBoard() {
+    ctx2.drawImage(board, 0, 0, bw, bh + (bh / 6));
+}
+
+function click(e) {
+//drop the chip where the user clicked
+    var offset = $(this).offset();
+    var xPos = (e.pageX - offset.left);
+    for (var i = 1; i < 8; i++) {
+        if (((i - 1) * (bw / 7)) < xPos && xPos < ((i) * (bw / 7))) {
+            dropChip(i);
+        }
+    }
+}
+
+function hoverChip(e) {
+    var offset = $(this).offset();
+    var xPos = (e.pageX - offset.left);
+    var image = new Image();
+    //checks which color's turn it is
+    if (moves % 2 === 0) {
+        image.src = "img/bestchipred.png";
+    }
+    else {
+        image.src = "img/bestchipblue.png";
+    }
+
+//draw the image of the chip to be dropped
+    for (var i = 1; i < 8; i++) {
+        if (xPos > ((i - 1) * (bw / 7)) && xPos < (i * (bw / 7)) && winner === "False") {
+            ctx.clearRect(0, -(bh / 6), bw, (bh / 6));
+            ctx.drawImage(image, ((i - 1) * (bw / 7)), -(bh / 6), (bw / 7), (bh / 6));
+        }
+    }
+
+//clear all draws of hover chips
+    if (winner === "True" && once === "False") {
+        ctx.clearRect(0, -(bh / 6), bw, (bh / 6));
+        once = "True";
     }
 }
